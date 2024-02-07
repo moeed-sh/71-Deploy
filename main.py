2f2c5489d6302305fdfbd99d52da84a8f5346423
mshari6 <shariatfar@gmail.com>
2024-02-06 18:03:52 -0600

    Use env variables and create a Procfile.

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ pip3 install -r requirements.txt
 This will install the packages from the requirements.txt for this project.
 '''
 
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
